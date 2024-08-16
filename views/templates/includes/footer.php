ebf3afc8fe60fb3440bb4b3f02ad05cdd0fc6e53
Nathalie <nat933@free.fr>
2024-08-16 11:57:47 +0200

    correction include

--- views/templates/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <a href="index.php?action=privacy">Politique de confidentialité</a>
-    <a href="index.php?action=legal">Mentions légales</a>
-    <a href="index.php">Tom Troc©</a>
-    <img src="./img/logo_tom_troc.png" alt="logo_tom_troc">
-</footer>
