b72f6500df1561dfe304b1337519ac61050b11be
NathHome <nat933@free.fr>
2025-01-04 15:29:22 +0100

    css connection /inscrition

--- views/templates/connectionForm.php
+++ views/templates/connectionForm.php
@@ -1,22 +1,22 @@
 <div class="connection-wrapper">
-    <h1>Connexion</h1>
-    <div class="connection-form">
-        <form action="index.php?action=connectUser" method="POST" class="foldedCorner">
-            <div class="formGrid">
-                <label for="email">Adresse email</label>
-                <input type="email" name="email" id="email" required>
-
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password" required>
-
-                <button class="submit">Se connecter</button>
-            </div>
-        </form>
+    <div class="connection-container">
+        <h1>Connexion</h1>
+        <div class="connection-form">
+            <form action="index.php?action=connectUser" method="POST" class="foldedCorner">
+                <div class="formGrid">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" id="email" required>
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" required>
+                    <button class="submit">S'inscrire</button>
+                </div>
+            </form>
+        </div>
+        <p class="login-link">
+            Pas de compte ? <a href="index.php?action=registrationForm">Inscrivez-vous</a>
+        </p>
     </div>
-    <p class="login-link">
-        Pas de compte ? <a href="index.php?action=registrationForm">Inscrivez-vous</a>
-    </p>
     <div class="home-image">
-        <img src="../../assets/img/hero.webp" alt="Image d'accueil">
+        <img src="../../assets/img/connexion.svg" alt="Image d'accueil">
     </div>
 </div>
--- views/templates/registrationForm.php
+++ views/templates/registrationForm.php
@@ -1,25 +1,24 @@
 <div class="registration-wrapper">
-    <h1>Inscription</h1>
-    <div class="registration-form">
-        <form action="index.php?action=registerUser" method="post" class="foldedCorner">
-            <div class="formGrid">
-                <label for="username">Pseudo</label>
-                <input type="text" name="username" id="username" autocomplete="false" required>
-
-                <label for="email">Adresse email</label>
-                <input type="email" name="email" id="email" autocomplete="false" required>
-
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password" autocomplete="false" required>
-
-                <button class="submit">S'inscrire</button>
-            </div>
-        </form>
+    <div class="registration-container">
+        <h1>Inscription</h1>
+        <div class="registration-form">
+            <form action="index.php?action=registerUser" method="post" class="foldedCorner">
+                <div class="formGrid">
+                    <label for="username">Pseudo</label>
+                    <input type="text" name="username" id="username" autocomplete="false" required>
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" id="email" autocomplete="false" required>
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" autocomplete="false" required>
+                    <button class="submit">S'inscrire</button>
+                </div>
+            </form>
+        </div>
+        <p class="login-link">
+            Déjà inscrit ? <a href="index.php?action=connectionForm">Connectez-vous</a>
+        </p>
     </div>
-    <p class="login-link">
-        Déjà inscrit ? <a href="index.php?action=connectionForm">Connectez-vous</a>
-    </p>
     <div class="home-image">
-        <img src="../../assets/img/hero.webp" alt="Image d'accueil">
+        <img src="../../assets/img/connexion.svg" alt="Image d'accueil">
     </div>
 </div>
